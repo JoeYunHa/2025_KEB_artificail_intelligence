5107ee15a5d5806fc5d987cf8760e2a58a0f8b08
JoeYunHa <12201806@inha.edu>
2025-02-27 17:19:13 +0900

    add day14_titanic_classifier

--- day12.py
+++ day12.py
@@ -1,5 +1,5 @@
 # Assingment
-# v1.1) 나이에 따른 생존율을 고려하여 모델을 선택하고
+# v1.1) 나이에 따른 생존율을 고려하여 모델을 선택, 학습 및 예측
 import numpy as np
 import pandas as pd
 from sklearn.impute import SimpleImputer
@@ -11,26 +11,42 @@ titanic = sns.load_dataset('titanic')
 
 # visualized to select model
 
-survived_data = titanic[titanic['survived'] == 1]
+survived_data = titanic[titanic['survived'] == 1].copy()
 
-dead_data = titanic[titanic['survived']==0]
+dead_data = titanic[titanic['survived']==0].copy()
+# .copy() => SettingWithCopyWarning 방지
 
-survived_data['age_group'] = pd.cut(survived_data['age'], bins=[0,10,20,30,40,50,60,70,80,90,100], labels=['0-9','10-19','20-29','30-39','40-49','50-59','60-69','70-79','80-89','90+'])
+titanic['age_group'] = pd.cut(titanic['age'],
+                            bins=[0,10,20,30,40,50,60,70,80,90,100],
+                            labels=['0-9','10-19','20-29','30-39','40-49','50-59','60-69','70-79','80-89','90+'])
+
+survived_data['age_group'] = pd.cut(survived_data['age'],
+                            bins=[0,10,20,30,40,50,60,70,80,90,100],
+                            labels=['0-9','10-19','20-29','30-39','40-49','50-59','60-69','70-79','80-89','90+'])
 dead_data['age_group'] = pd.cut(dead_data['age'], bins=[0,10,20,30,40,50,60,70,80,90,100], labels=['0-9','10-19','20-29','30-39','40-49','50-59','60-69','70-79','80-89','90+'])
 
-fig, ax = plt.subplots(figsize=(12, 6))
+age_group_total = titanic['age_group'].value_counts().sort_index()
+age_group_dead = dead_data['age_group'].value_counts().sort_index()
+age_group_survived = survived_data['age_group'].value_counts().sort_index()
 
-x = np.arange(len(survived_data['age_group'].value_counts().index))
-width = 0.35
+death_rate = (age_group_dead / age_group_total) * 100
+survived_rate = (age_group_survived / age_group_total) * 100
 
-ax.bar(x - width/2, survived_data['age_group'].value_counts().sort_index().values, width, label='생존자', alpha=0.5, color='blue')
-ax.bar(x + width/2, dead_data['age_group'].value_counts().sort_index().values, width, label='사망자', alpha=0.5, color='red')
+plt.figure(figsize=(12,6))
+sns.set_style("whitegrid")
 
-ax.set_xticks(x)
-ax.set_xticklabels(survived_data['age_group'].value_counts().index)
-ax.set_xlabel('age')
-ax.set_ylabel('counts')
+# death_rate_bars = plt.bar(death_rate.index, death_rate.values, color='red')
+survived_rate_bars = plt.bar(survived_rate.index, survived_rate.values, color='blue')
 
-ax.legend()
+plt.xlabel('age', fontsize = 16)
+plt.ylabel('survived_rate (%)' , fontsize=12)
+plt.ylim(0,100)
 
+for bar in survived_rate_bars:
+    height = bar.get_height()
+    plt.text(bar.get_x() + bar.get_width() / 2., height,
+             f'{height:.1f}%', ha='center', va='bottom')
+
+plt.tight_layout()
 plt.show()
+
