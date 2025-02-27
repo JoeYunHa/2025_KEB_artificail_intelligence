e7249d41cf7b39497976566cf78bdd41134b6012
JoeYunHa <12201806@inha.edu>
2025-02-27 10:52:20 +0900

    v1.0 visualized day12.py

--- day12.py
+++ day12.py
@@ -1,29 +1,36 @@
 # Assingment
-# v0.9) v0.8 파일의 결측치 값을 산술평균으로 채워 넣는 방법을 적용하시오.
+# v1.1) 나이에 따른 생존율을 고려하여 모델을 선택하고
 import numpy as np
 import pandas as pd
 from sklearn.impute import SimpleImputer
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+import seaborn as sns
 
-df = pd.DataFrame(
-    {
-        'A':[1,2,np.nan,4],
-        'B':[np.nan,12,3,4],
-        'C':[1,2,3,4]
-    }
-)
-print(df)
+titanic = sns.load_dataset('titanic')
 
-# using option 3 -> median
-A_median = df['A'].median()
-B_median = df['B'].median()
-df['A'].fillna(A_median, inplace=True)
-df['B'].fillna(B_median, inplace=True)
+# visualized to select model
 
-print("using fillna()")
-print(df)
+survived_data = titanic[titanic['survived'] == 1]
 
-# using SimpleImputer
-imputer = SimpleImputer(strategy='mean')
-df[['A','B']] = imputer.fit_transform(df[['A','B']])
+dead_data = titanic[titanic['survived']==0]
 
-print(df)
+survived_data['age_group'] = pd.cut(survived_data['age'], bins=[0,10,20,30,40,50,60,70,80,90,100], labels=['0-9','10-19','20-29','30-39','40-49','50-59','60-69','70-79','80-89','90+'])
+dead_data['age_group'] = pd.cut(dead_data['age'], bins=[0,10,20,30,40,50,60,70,80,90,100], labels=['0-9','10-19','20-29','30-39','40-49','50-59','60-69','70-79','80-89','90+'])
+
+fig, ax = plt.subplots(figsize=(12, 6))
+
+x = np.arange(len(survived_data['age_group'].value_counts().index))
+width = 0.35
+
+ax.bar(x - width/2, survived_data['age_group'].value_counts().sort_index().values, width, label='생존자', alpha=0.5, color='blue')
+ax.bar(x + width/2, dead_data['age_group'].value_counts().sort_index().values, width, label='사망자', alpha=0.5, color='red')
+
+ax.set_xticks(x)
+ax.set_xticklabels(survived_data['age_group'].value_counts().index)
+ax.set_xlabel('age')
+ax.set_ylabel('counts')
+
+ax.legend()
+
+plt.show()
